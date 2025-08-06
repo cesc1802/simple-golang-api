0a8aacab8420db68bd2f627476ecef4abede44dd
Cesc <thuocnv1802@gmail.com>
2025-08-06 12:38:47 +0700

    err handle

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
+	"os"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run(":3000"); err != nil {
+		os.Exit(1)
+	}
 }
